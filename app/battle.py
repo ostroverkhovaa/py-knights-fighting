47ccf7013c8da995d2375d42b5ea1d39a8887b65
Anastasiia <skubakovaa@gmail.com>
2024-12-03 20:53:07 +0100

    Solution

---
+++ app/battle.py
@@ -0,0 +1,15 @@
+from app.knights import Knight
+
+
+class Battle:
+    @staticmethod
+    def knights_battle(knight1: Knight, knight2: Knight) -> None:
+        knight1.hp -= knight2.power - knight1.protection
+        knight2.hp -= knight1.power - knight2.protection
+
+        # check if someone fell in battle
+        if knight1.hp <= 0:
+            knight1.hp = 0
+
+        if knight2.hp <= 0:
+            knight2.hp = 0
---
+++ app/knights.py
@@ -0,0 +1,33 @@
+class Knight:
+    def __init__(
+            self,
+            name: str,
+            power: int,
+            hp: int,
+            armour: list,
+            weapon: dict,
+            potion: dict | None,
+            protection: int = 0) -> None:
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.armour = armour
+        self.weapon = weapon
+        self.potion = potion
+        self.protection = protection
+
+    def preparation(self) -> None:
+        self.power += self.weapon["power"]
+
+        if self.potion is not None:
+            if "power" in self.potion["effect"]:
+                self.power += self.potion["effect"]["power"]
+
+            if "protection" in self.potion["effect"]:
+                self.protection += self.potion["effect"]["protection"]
+
+            if "hp" in self.potion["effect"]:
+                self.hp += self.potion["effect"]["hp"]
+
+        for current_armour in self.armour:
+            self.protection += current_armour["protection"]
